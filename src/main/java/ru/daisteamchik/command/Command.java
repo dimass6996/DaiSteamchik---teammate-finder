f7969e7034dde891d33d78bcacd564738cb6afb9
dimass6996 <boss.temirsos33@gmail.com>
2026-09-19 22:13:25 +0500

    writed command interface

---
+++ src/main/java/ru/daisteamchik/command/Command.java
@@ -0,0 +1,9 @@
+package ru.daisteamchik.command // это чтобы компилятор смог понять полный путь к файлу
+
+public interface Command {
+    String getName();
+    String getDescription();
+    String execute(String[] args);  // создали интерфейс каждой команды, что-то типа h Файла в c++
+                                    // подключается в классе через implements Command - класс
+                                    // должен иметь поля которые содержатся в интерфейсе обязательно
+}
